dc6489196d2d694cbbf64b675167a715ce1359fa
Jainam doshi <jainamd27@gmail.com>
2026-06-27 16:28:23 +0530

    Implement Supabase integration: add supabase client and update Landing page for session handling

--- yuvalink/src/pages/Landing.jsx
+++ yuvalink/src/pages/Landing.jsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
+import { supabase } from "../config/supabase";
+
 function Landing() {
-  return (
-    <div>Landing</div>
-  )
+  useEffect(() => {
+    async function testConnection() {
+      const { data, error } = await supabase.auth.getSession();
+
+      console.log("Data:", data);
+      console.log("Error:", error);
+    }
+
+    testConnection();
+  }, []);
+
+  return <div>YuvaLink</div>;
 }
 
-export default Landing
+export default Landing;
